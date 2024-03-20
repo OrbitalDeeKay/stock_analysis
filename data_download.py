e9d92f09b048059fb6d815c8de30747a91b55f90
Archi <tiomka36@gmail.com>
2024-03-20 23:36:54 +0300

    Added def notify_if_strong_fluctuations

--- data_download.py
+++ data_download.py
@@ -15,5 +15,16 @@ def add_moving_average(data, window_size=5):
 
 def calculate_and_display_average_price(data):
     average_price = round(data['Close'].mean(), 2)  # Price will be rounded up the second decimal
-    print(Fore.BLUE + 'Средняя цена за период:', average_price)
-    print(Fore.RESET)
+    print(Fore.BLUE + 'Средняя цена за период:', average_price, Fore.RESET)
+    # print(Fore.RESET)
+
+
+def notify_if_strong_fluctuations(data, threshold=10):
+    max_price = data['Close'].max()
+    min_price = data['Close'].min()
+    price_diff = max_price - min_price
+    price_change_percent = round((price_diff / min_price) * 100, 2)
+
+    if price_change_percent > threshold:
+        print(Fore.RED + f"Предупреждение: Сильные колебания в цене акций ({price_change_percent}% разницы)",
+              Fore.RESET)
--- main.py
+++ main.py
@@ -7,8 +7,9 @@ def main():
     print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
     print("Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
 
-    ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
+    ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc): »")
     period = input("Введите период для данных (например, '1mo' для одного месяца): ")
+    threshold = float(input("Введите порог колебаний акций в % (например, '10' для десяти процентов): "))
 
     # Fetch stock data
     stock_data = dd.fetch_stock_data(ticker, period)
@@ -18,6 +19,9 @@ def main():
 
     # Calculate and display average price of a stock
     dd.calculate_and_display_average_price(stock_data)
+
+    # Strong fluctuations notification
+    dd.notify_if_strong_fluctuations(stock_data, threshold)
 
     # Plot the data
     dplt.create_and_save_plot(stock_data, ticker, period)
